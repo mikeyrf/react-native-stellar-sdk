af7b6941332e1c2540718cf3ed8dd84d74815e20
mikeyrf <mikeyrf@users.noreply.github.com>
2019-04-09 19:59:40 -0700

    Use @mikeyrf/react-native-event-source

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import {randomBytesAsync} from './crypto';
-import StellarSdk, {Keypair} from 'stellar-sdk';
+import { randomBytesAsync } from './crypto';
+import StellarSdk, { Keypair } from 'stellar-sdk';
 
 Keypair.randomAsync = function randomAsync() {
-    return randomBytesAsync(32)
-        .then(secret => Keypair.fromRawEd25519Seed(secret));
+  return randomBytesAsync(32)
+    .then(secret => Keypair.fromRawEd25519Seed(secret));
 };
 
 Keypair.random = function random() {
-    throw new Error('Use Stellar.Keypair.randomAsync for React Native');
+  throw new Error('Use Stellar.Keypair.randomAsync for React Native');
 };
 
-module.exports = {...StellarSdk, randomBytesAsync};
+module.exports = { ...StellarSdk, randomBytesAsync };
